f78c68eeb8cce186245ce52af6fe745fc8d51130
vizin <alvaro.nunes15@gmail.com>
2023-03-08 17:04:38 -0300

    feat: Modificado o método para ligar apenas paara o telefone preto

--- src/br/com/newton/linguagem/aula03/Agenda.java
+++ src/br/com/newton/linguagem/aula03/Agenda.java
@@ -5,20 +5,34 @@ public class Agenda {
         Telefone telefone = new Telefone();
         Contato contato1 = new Contato();
         Contato contato2 = new Contato();
+        Contato contato3 = new Contato();
+        Contato contato4 = new Contato();
+
         contato1.nome = "Rafaela";
         contato1.numero = "123456";
-        telefone.marca = "Iphone";
         telefone.cor = "Vermelho";
+        telefone.cor2 = "Preto";
         contato2.nome = "João";
         contato2.numero = "318989952";
+        contato3.nome = "Carmen";
+        contato3.numero = "3189871147";
+        contato4.nome = "Juliana";
 
+        telefone.marca = "Iphone";
+        telefone.marca2 = "S20";
 
         telefone.contato = contato1;
         telefone.contatoA = contato2;
+        telefone.contatoB = contato3;
+        telefone.contatoC = contato4;
+
         telefone.Tocar(" Pagode");
+        telefone.Ligar("");
+
 
         System.out.println(telefone.contato.nome);
         System.out.println(telefone.contatoA.nome + ", " + telefone.marca + ", " + telefone.contatoA.numero);
+        System.out.println(telefone.contatoB.nome + ", " + telefone.marca2 + ", " + telefone.contatoB.numero);
 
     }
 }
--- src/br/com/newton/linguagem/aula03/Telefone.java
+++ src/br/com/newton/linguagem/aula03/Telefone.java
@@ -3,11 +3,25 @@ package br.com.newton.linguagem.aula03;
 public class Telefone {
 
     public String marca;
+    public String marca2;
     public String cor;
 
+    public String cor2;
+
     public void Tocar(String estiloMusical){
+
         System.out.println("Tocando" + estiloMusical);
+
+    }
+
+        public void Ligar (String ligação){
+        if (cor2 == "Preto") {
+            System.out.println("Ligando" + ligação);
+        }
+
     }
     Contato contato;
     Contato contatoA;
+    Contato contatoB;
+    Contato contatoC;
 }
